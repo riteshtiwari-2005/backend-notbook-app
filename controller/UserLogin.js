650610c80ea3eda3f64b8d46ec4361553aeac254
riteshtiwari-2005 <ritesht378@gmail.com>
2025-06-15 12:27:42 +0530

    G

--- controller/UserLogin.js
+++ controller/UserLogin.js
@@ -1,103 +1,96 @@
-const User=require("../model/User")
-const bycrypt=require("bcryptjs")
-const jsonwebtoken=require("jsonwebtoken")
-exports.Signup=async(req,res)=>{
-try{
-const {name,email,password}=req.body
+const User = require("../model/User");
+const bycrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+exports.Signup = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
 
-if(!name || !email|| !password)
-{
-    return res.status(401).json({
-        success:false,
-        message:"Filed Not Should Empty"
-    })
-}
+    if (!name || !email || !password) {
+      return res.status(401).json({
+        success: false,
+        message: "Filed Not Should Empty",
+      });
+    }
 
-const checkuser=await User.findOne({email})
-if(checkuser)
-{
-   return res.status(401).json({
-        success:false,
-        message:"User Already Exist"
-    })  
-}
+    const checkuser = await User.findOne({ email });
+    if (checkuser) {
+      return res.status(401).json({
+        success: false,
+        message: "User Already Exist",
+      });
+    }
 
-const hashpassword=await bycrypt.hash(password,10)
-const UserSaved=await User.create({
-    name,
-    email,
-    password:hashpassword
-})
+    const hashpassword = await bycrypt.hash(password, 10);
+    const UserSaved = await User.create({
+      name,
+      email,
+      password: hashpassword,
+    });
 
-return res.status(200).json({
-    success:true,
-    UserSaved,
-    message:"User Signup Success"
-})
-}
+    return res.status(200).json({
+      success: true,
+      UserSaved,
+      message: "User Signup Success",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
-catch(error)
-{
-return res.status(500).json({
-        success:false,
-        message:error.message
-    })  
-}
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
+    // Check for empty fields
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Fields should not be empty",
+      });
+    }
 
+    // Check if user exists
+    const checkuser = await User.findOne({ email });
+    if (!checkuser) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found with this email",
+      });
+    }
 
-}
+    // Compare password
+    const isMatch = await bcrypt.compare(password, checkuser.password);
+    if (!isMatch) {
+      return res.status(401).json({
+        success: false,
+        message: "Password is invalid",
+      });
+    }
 
-exports.login=async(req,res)=>{
-try{
-const {email,password}=req.body
-if(!email || !password)
-{
-    return res.status(401).json({
-        success:false,
-        message:"Filed Not Should Empty"
-    })
-}
+    // Prepare token payload without password
+    const payload = {
+      id: checkuser._id,
+      email: checkuser.email,
+      name: checkuser.name,
+    };
 
-const checkuser=await User.findOne({email})
-if(!checkuser)
-{
-   return res.status(401).json({
-        success:false,
-        message:"User Not Found with this email"
-    })  
-}
-if(bycrypt.compare(password,checkuser.password))
-{
-    checkuser.password=null
-const token=jsonwebtoken.sign({checkuser},process.env.JWT_KEY,{expiresIn:'1h'})
-return res.status(200).json({
-    success:true,
-    token,
-    message:"Login Success"
-})
+    // Sign token
+    const token = jwt.sign(payload, process.env.JWT_KEY, { expiresIn: "1h" });
 
-
-}
-
-else
-{
-     return res.status(401).json({
-        success:false,
-        message:"Password is Invalid"
-    })  
-}
-
-
-}
-
-catch(error)
-{
-  return res.status(500).json({
-        success:false,
-        message:error.message
-    })    
-}
-
-
-}
+    // Send response
+    return res.status(200).json({
+      success: true,
+      token,
+      message: "Login success",
+    });
+  } catch (error) {
+    console.error("Login Error:", error);
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
